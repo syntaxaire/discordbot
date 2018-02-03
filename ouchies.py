8b71416672b4b02f66fb3bb3e9f0b53ae9941f85
Fart C Annon <butts@fartcannon.com>
2018-02-03 03:30:52 -0500

    store deaths properly, ouchies? command

--- ouchies.py
+++ ouchies.py
@@ -4,18 +4,21 @@ import json
 class Oww:
 
     def load(self):
+        self.dt={}
         try:
             with open('ouchies.txt') as f:
                 return json.load(f)
         except Exception:
                 pass
+
+
     def __init__(self):
         self.dt=self.load()
+
 
     def save(self):
         with open('ouchies.txt', 'w') as f:
             json.dump(self.dt, f, ensure_ascii=False)
-
 
 
     def msg(self):
@@ -32,9 +35,10 @@ class Oww:
                     i=i+1
             return cmsg
 
+
     def record(self, message):
-        m = message.partition(' ')
-        if m[0] not in self.dt:
-            self.dt[m[0]] = 0
-        self.dt[m[0]] = self.dt[m[0]] + 1
+        m = message.split()
+        if m[1] not in self.dt:
+            self.dt[m[1]] = 0
+        self.dt[m[1]] = self.dt[m[1]] + 1
         self.save()
