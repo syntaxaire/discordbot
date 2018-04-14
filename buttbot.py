a88036874a690de33fc3c32a7de73d7b09c65761
Fart Butt <butts@fartcannon.com>
2018-04-14 01:29:18 -0400

    set up for dispatching per-channel

--- buttbot.py
+++ buttbot.py
@@ -1,44 +1,41 @@
-from vacuum import Vacuum
-import mojang
 import discord_comms
-import asyncio
-
+import mojang
+from vacuum import Vacuum
 
 
 class buttbot:
-    def __init__(self,BotObject):
-        self.vacuum=Vacuum()
-        self.comm=discord_comms.discord_comms()
-        self.discordBot=BotObject
+    def __init__(self, BotObject):
+        self.vacuum = Vacuum()
+        self.comm = discord_comms.discord_comms()
+        self.discordBot = BotObject
 
-    def lastseen(self,player):
+    def lastseen(self, player):
         try:
             if player:
                 returnz = self.vacuum.lastseen(player)
                 if returnz:
                     return returnz
         except IndexError:
-            return("who am i looking for?")
+            return ("who am i looking for?")
 
     def mojang(self):
         msg = mojang.mojang_status_requested()
         for t in msg:
             return t
 
-
-    async def dispatch(self,message):
-        tokenDict={"mojang":self.mojang}
+    async def dispatch(self, message):
+        tokenDict = {"mojang": self.mojang}
         try:
             command = message.content.split("&", 1)[1]
         except IndexError:
-            #no & found in message.
+            # no & found in message.
             command = ''
         if command:
             c2 = command.split(' ')
-            func=tokenDict[c2[0]]
-            back=func()
+            func = tokenDict[c2[0]]
+            back = func()
             if back:
-                await self.doComms(back,message.channel)
+                await self.doComms(back, message.channel)
 
-    async def doComms(self,message,channel):
-        await self.comm.do_send_message(channel,self.discordBot,message)
+    async def doComms(self, message, channel):
+        await self.comm.do_send_message(channel, self.discordBot, message)
