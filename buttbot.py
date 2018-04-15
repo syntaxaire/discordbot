02b3617b0e70369e7b49f935cd9a7bc26f8bb3e4
Fart Butt <butts@fartcannon.com>
2018-04-15 00:42:15 -0400

    dispatch by module

--- buttbot.py
+++ buttbot.py
@@ -1,30 +1,29 @@
 import discord_comms
 import mojang as mj
+from butt_database import db
 from vacuum import Vacuum
 from wordreplacer import WordReplacer
-from butt_database import db
 
 
 class buttbot:
     def __init__(self, BotObject):
-        self.db=db()
+        self.db = db()
         self.vacuum = Vacuum(self.db)
         self.comm = discord_comms.discord_comms()
         self.discordBot = BotObject
-        self.used={}
-        self.shitpost=WordReplacer()
-        self.mojang=mj.mojang()
-        self.allcommands={}
+        self.used = {}
+        self.shitpost = WordReplacer()
+        self.mojang = mj.mojang()
+        self.allcommands = {}
 
-    def do_nltk(self,message):
+    def do_nltk(self, message):
         if str(message.author) in self.channel_admins:
-            return(self.shitpost.wordtagger(message.content))
+            return (self.shitpost.wordtagger(message.content))
 
     def do_howchies(self):
         print("fuck")
 
     async def dispatch(self, message):
-
         try:
             command = message.content.split("&", 1)[1]
         except IndexError:
@@ -34,13 +33,13 @@ class buttbot:
             command, se, arguments = command.partition(' ')
 
             if command in self.vacuum.return_commands():
-                module=self.vacuum
+                module = self.vacuum
             try:
-                func=getattr(module, 'do_' + command)
+                func = getattr(module, 'do_' + command)
             except AttributeError:
-                #TODO: probably should build a default return all the command thing here.
+                # TODO: probably should build a default return all the command thing here.
                 pass
-            back=func(arguments)
+            back = func(arguments)
             if back:
                 await self.doComms(back, message.channel)
 
