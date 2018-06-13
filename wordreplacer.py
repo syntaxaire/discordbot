6c0649d1cbac8a2f30abee920463e655c39d6f83
Fart Butt <butts@fartcannon.com>
2018-06-13 00:13:38 -0400

    first pass of secondary tag checking

--- wordreplacer.py
+++ wordreplacer.py
@@ -1,20 +1,11 @@
 import json
 import time
 from random import *
-import os
-import config
 
 import nltk
 from nltk.stem import WordNetLemmatizer
 
 from butt_library import *
-
-#########################################
-#   environ vars for stanford coreNLP   #
-#########################################
-os.environ['STANFORD_PARSER'] = '/path/to/standford/jars'
-os.environ['STANFORD_MODELS'] = '/path/to/standford/jars'
-#########################################
 
 
 class WordReplacer:
@@ -24,9 +15,6 @@ class WordReplacer:
         self.timer = timer
         self.used = {}
         self.command = {"nltk": 'wordreplacer'}
-        # NLTK test
-        # self.nouns=set()
-        # self.nouns = {x.name().split('.', 1)[0] for x in wn.all_synsets('n')}
 
     def load(self):
         try:
@@ -74,8 +62,6 @@ class WordReplacer:
             self.used['reverseshitpost'] = time.time()
             for t in self.wlist:  # replace everything aaaaaaa
                 message = message.replace('butt', self.wlist[randint(0, len(self.wlist) - 1)], 1)
-                # NLTK test
-                # message = message.replace('butt', sample(self.nouns,1)[0].replace('_'," "), 1)
             return message
 
     def wordtagger(self, message):
@@ -105,27 +91,67 @@ class WordReplacer:
                 # It is not sent to the word classifier to prevent a bunch of silly issues
                 nouns = self.wordclassifier(strip_IRI(message.split(" ", 1)[1]), author)
             else:
-                nouns = self.wordclassifier(strip_IRI(message), author)
+                print(":::new sentence: %s" % self.wordtagger(message))
+                nouns2 = self.wordclassifier(strip_IRI(message), author)
+                print("old tag: %s" % nouns2)
+                nouns = self.findNounsBySecondaryTag(self.wordtagger(strip_IRI(message)))
             # list comprehension to remove words that shouldn't be included in the list
-            badwords = ['i', 'gon', 'beat', 'dont', 'lol', 'yeah','tho']
+            badwords = ['i', 'gon', 'beat', 'dont', 'lol', 'yeah', 'tho']
 
             nouns = [var for var in nouns if var not in badwords]
-            if len(nouns) > 1:
-                if randint(1, 5) == 3:
-                    if ('shitpost' not in self.used or time.time() - self.used['shitpost'] > self.timer):
-                        self.used['shitpost'] = time.time()
-                        lemmatizer = WordNetLemmatizer()
-                        buttword = randint(0, len(nouns) - 1)  # this is the word we are replacing with butt.
-                        if lemmatizer.lemmatize(nouns[buttword]) is not nouns[buttword]:
-                            # the lemmatizer thinks that this is a plural
-                            return unedited_message.replace(nouns[buttword], self.buttInProperCase(nouns[buttword],'butts'))
-                        else:
-                            return unedited_message.replace(nouns[buttword], self.buttInProperCase(nouns[buttword],'butt'))
 
-    def buttInProperCase(self,wordToButt,buttToReplace):
+            if randint(3, 3) == 3: #TODO: fix for prod
+                if 'shitpost' not in self.used or time.time() - self.used['shitpost'] > self.timer:
+                    self.used['shitpost'] = time.time()
+                    lemmatizer = WordNetLemmatizer()
+                    buttword = randint(0, len(nouns) - 1)  # this is the word we are replacing with butt.
+                    if lemmatizer.lemmatize(nouns[buttword]) is not nouns[buttword]:
+                        # the lemmatizer thinks that this is a plural
+                        return unedited_message.replace(nouns[buttword],
+                                                        self.buttInProperCase(nouns[buttword], 'butts'))
+                    else:
+                        return unedited_message.replace(nouns[buttword],
+                                                        self.buttInProperCase(nouns[buttword], 'butt'))
+
+    def buttInProperCase(self, wordToButt, buttToReplace):
         if wordToButt.istitle():
             return buttToReplace.title()
         elif wordToButt.isupper():
             return buttToReplace.upper()
         else:
             return buttToReplace
+
+    def findNounsBySecondaryTag(self, taggedSentence):
+        # we are going to look for a few speech patterns:
+        # (the) noun
+        index = 0
+        nounList = []
+        for a in taggedSentence:
+            try:
+                nextWord = taggedSentence[index + 1]
+            except IndexError:
+                # nextword is empty, dont give a crap about this
+                nextWord = None
+            wordTagsToCheckPrioritize = ['PRP','PRP$']
+            wordTagsToCheckForNoun = ['DT', 'JJ', 'JJS', 'JJR', 'CD']
+            wordsThatMayBeMispelled = ['teh']
+            tagsToAcceptAsNouns = ['NN', 'NNS']
+            if a[1] in wordTagsToCheckForNoun or a[0] in wordsThatMayBeMispelled:
+                # we want to check adjectives and determiners
+                try:
+                    if nextWord[1] in tagsToAcceptAsNouns:
+                        # word has a noun tag
+                        nounList.append(nextWord[0])
+                except TypeError:
+                    # tag is empty for this word
+                    pass
+
+            index += 1
+        print("new tag: %s" % nounList)
+        return nounList
+
+    def buttWord(self,word):
+        pass
+
+    def wordReplacer(self,word):
+        pass
