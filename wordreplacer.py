ff4f3d1cfb09edae6bd1cc169f8a2a8c2020bf90
Fart Butt <butts@fartcannon.com>
2018-06-15 00:41:19 -0400

    return if the sentence has a targeted word for butting

--- wordreplacer.py
+++ wordreplacer.py
@@ -100,7 +100,7 @@ class WordReplacer:
             badwords = ['i', 'gon', 'beat', 'dont', 'lol', 'yeah', 'tho', '>']
             nouns = [var for var in nouns if var not in badwords]
 
-            if len(nouns) > 1:
+            if len(nouns) > 1 or (len(nouns) > 0 and targeted == True):
                 if randint(1, 5) == 3:
                     if 'shitpost' not in self.used or time.time() - self.used['shitpost'] > self.timer:
                         self.used['shitpost'] = time.time()
