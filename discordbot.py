7f71903a4af0b7f5d5171dc67dc7ad6b1933677d
Fart Butt <butts@fartcannon.com>
2018-07-05 19:11:18 -0400

    formatting

--- discordbot.py
+++ discordbot.py
@@ -16,12 +16,13 @@ command_channels = {}
 
 if test_environment == True:
     command_channels["408168696834424832"] = buttbot(client, "development.ini", db_, db_secrets[0], db_secrets[1],
-                                                     stat_module)
+                                                     stat_module, True)
     command_channels["199981748098957312"] = buttbot(client, "DPT_document.ini", db_, db_secrets[0], db_secrets[1],
-                                                     stat_module)
+                                                     stat_module, True)
 else:
     for i in channel_configs:
-            command_channels[i.split("/")[1][:-4]] = buttbot(client, i, db_, db_secrets[0], db_secrets[1], stat_module)
+        command_channels[i.split("/")[1][:-4]] = buttbot(client, i, db_, db_secrets[0], db_secrets[1], stat_module,
+                                                         False)
 
 
 @client.event
