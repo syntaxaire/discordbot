eedf8668afc97c614a79badaa654bbd2ea5972d5
Fart Butt <butts@fartcannon.com>
2018-07-24 21:00:02 -0400

    first poast

---
+++ butt_config.py
@@ -0,0 +1,25 @@
+import configparser
+
+
+class butt_config():
+
+    def __init__(self, conf):
+        self.config_file = configparser.ConfigParser()
+        self.config_file.read_file(open(conf))
+
+    def get(self, section, key):
+        # passthrough function for backwards compatibility
+        return self.config_file.get(section, key)
+
+    def getboolean(self, section, key):
+        # passthrough function for backwards compatibility
+        return self.config_file.getboolean(section, key)
+
+    def get_all_whitelisted_bots(self):
+        return self.process_list_from_configparser('discordbot', 'whitelisted_bots')
+
+    def get_all_emojis(self):
+        return self.get('discordbot', 'butt_response_emojis').split(",")
+
+    def process_list_from_configparser(self, section, key):
+        return self.get(section, key).split(",")
